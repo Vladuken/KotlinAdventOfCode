b64a5f7e39a2bc0c2029aca43838778a401870c8
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2022-12-01 22:42:18 +0300

    Add Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
+
+    fun prepareListOfElfsWithCalories(input: List<String>): List<List<Int>> {
+        return input
+            // Null is for empty strings that are separators.
+            .map { it.toIntOrNull() }
+            .fold(mutableListOf(mutableListOf<Int>())) { listOfElfs, product ->
+                if (product == null) {
+                    listOfElfs.add(mutableListOf())
+                } else {
+                    listOfElfs.last().add(product)
+                }
+                listOfElfs
+            }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return prepareListOfElfsWithCalories(input)
+            .maxOfOrNull { it.sum() }
+            ?: error("Illegal state")
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return prepareListOfElfsWithCalories(input)
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
