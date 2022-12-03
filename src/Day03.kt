598365b786b185e5bdfc9e108ac232a74476d386
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2022-12-03 08:23:25 +0300

    Day 3 solution
    - and prepare for day 4

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,19 +1,37 @@
+const val ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { s1 ->
+                val mid: Int = s1.length / 2
+                s1.substring(0, mid) to s1.substring(mid)
+            }
+            .sumOf { (s1, s2) ->
+                val union = s1.toSet().intersect(s2.toSet())
+                union.sumOf { ALPHABET.indexOf(it).inc() }
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowSize = 3
+        return input
+            .windowed(windowSize, windowSize)
+            .sumOf { rucksacks ->
+                val unique = rucksacks
+                    .map { it.toSet() }
+                    .reduce { init, item -> init.intersect(item) }
+                unique.sumOf { ALPHABET.indexOf(it).inc() }
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    val part1Test = part1(testInput)
+    val part2Test = part2(testInput)
 
-    //println(part1Test)
-    check(part1Test == 1)
+    println(part2Test)
+    check(part2Test == 70)
 
     val input = readInput("Day03")
     println(part1(input))
---
+++ src/Day04.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    val part1Test = part1(testInput)
+
+    //println(part1Test)
+    check(part1Test == 1)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
