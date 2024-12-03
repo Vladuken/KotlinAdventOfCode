7e45e3921148eb1c4f79faa16801eea40120a37c
Uladzislau Petrushkevich <upetrushkevich@pl-m-vpetrushkevich.local>
2024-12-03 06:10:55 +0100

    Year 2024 Day 3

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 4 ⭐
+  2024 - 6 ⭐
 </h1>
 <a href="src/year2024/01/Day01.kt">
   <img src="Media/2024/01.png" width="161px">
--- src/year2024/03/Day03.kt
+++ src/year2024/03/Day03.kt
@@ -5,20 +5,54 @@ import readInput
 private const val CURRENT_DAY = "03"
 
 
-private fun parseLineInto(
+private fun parseLineIntoValidConditions(
     line: String,
-): Any {
-    return 0
+): Int {
+    val conditions = line.split("mul(", ")")
+    val validConditions = conditions.mapNotNull {
+        val splitList = runCatching { it.split(",").map { it.toInt() } }
+            .getOrNull()
+        splitList
+    }
+
+    return validConditions.findSumOfValid()
+}
+
+private fun List<List<Int>>.findSumOfValid(): Int {
+    return this.sumOf {
+        val left = it[0]
+        val right = it[1]
+        left * right
+    }
+}
+
+
+private fun parseLineIntoValidConditionsAndDoDont(line: String): Int {
+    val resLine = line.split("do()", "don't()")
+    var currentLine = line
+    var counter = 0
+    resLine.forEach {
+        val subsrtingbefore = currentLine.substringBefore(it, "")
+        when (subsrtingbefore) {
+            "" -> counter += parseLineIntoValidConditions(it)
+            "don't()" -> {}
+            "do()" -> counter += parseLineIntoValidConditions(it)
+        }
+        currentLine = currentLine.substringAfter(it)
+    }
+    return counter
 }
 
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        val resString = input.joinToString("", "", "")
+        return parseLineIntoValidConditions(resString)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val resString = input.joinToString("", "", "")
+        return parseLineIntoValidConditionsAndDoDont(resString)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -26,21 +60,20 @@ fun main() {
 
     val part1Test = part1(testInput)
     println(part1Test)
-    check(part1Test == 1)
+    check(part1Test == 161)
 
-//    val part2Test = part2(testInput)
-//    println(part2Test)
-//    check(part2Test == 1)
-
+    val part2Test = part2(testInput)
+    println(part2Test)
+    check(part2Test == 48)
     val input = readInput("Day$CURRENT_DAY")
 
     // Part 1
     val part1 = part1(input)
     println(part1)
-    check(part1 == 1)
+    check(part1 == 187825547)
 
     // Part 2
     val part2 = part2(input)
     println(part2)
-    check(part2 == 1)
+    check(part2 == 85508223)
 }
