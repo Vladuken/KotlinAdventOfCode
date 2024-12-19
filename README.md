52ef825c86745cf34bf6a62b77921a9a0e6dbbae
Uladzislau Petrushkevich <upetrushkevich@pl-m-vpetrushkevich.local>
2024-12-19 06:33:13 +0100

    Year 2024 Day 19

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 36 ⭐
+  2024 - 38 ⭐
 </h1>
 <a href="src/year2024/01/Day01.kt">
   <img src="Media/2024/01.png" width="161px">
@@ -57,6 +57,9 @@
 </a>
 <a href="src/year2024/18/Day18.kt">
   <img src="Media/2024/18.png" width="161px">
+</a>
+<a href="src/year2024/19/Day19.kt">
+  <img src="Media/2024/19.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐
--- src/year2024/19/Day19.kt
+++ src/year2024/19/Day19.kt
@@ -5,20 +5,53 @@ import readInput
 private const val CURRENT_DAY = "19"
 
 
-private fun parseLineInto(
-    line: String
-): Any {
-    return 0
+private fun parseLineIntoPatterns(
+    line: String,
+): List<String> {
+    return line.split(",").map { it.trim() }
+}
+
+private fun countPossibleVariants(
+    patterns: List<String>,
+    remaining: String,
+    cache: MutableMap<String, Long>,
+): Long {
+    if (remaining.isEmpty()) return 1
+    val result = cache[remaining]
+    if (result != null) return result
+
+    var count = 0L
+    patterns.forEach {
+        if (remaining.startsWith(it)) {
+            count += countPossibleVariants(patterns, remaining.removePrefix(it), cache)
+        }
+    }
+
+    cache[remaining] = count
+    return count
 }
 
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        val patterns = parseLineIntoPatterns(input.first())
+        val items = input.reversed().takeWhile { it.isNotBlank() }.reversed()
+
+        val cache = mutableMapOf<String, Long>()
+        val totalCount = items.count {
+            countPossibleVariants(patterns, it, cache) != 0L
+        }
+        return totalCount
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val patterns = parseLineIntoPatterns(input.first())
+        val items = input.reversed().takeWhile { it.isNotBlank() }.reversed()
+        val totalCount = items.sumOf {
+            val cache = mutableMapOf<String, Long>()
+            countPossibleVariants(patterns, it, cache)
+        }
+        return totalCount
     }
 
     // test if implementation meets criteria from the description, like:
@@ -26,21 +59,22 @@ fun main() {
 
     val part1Test = part1(testInput)
     println(part1Test)
-    check(part1Test == 1)
+    check(part1Test == 6)
 
-//    val part2Test = part2(testInput)
-//    println(part2Test)
-//    check(part2Test == 1)
+    val part2Test = part2(testInput)
+    println(part2Test)
+    check(part2Test == 16L)
 
     val input = readInput("Day$CURRENT_DAY")
 
     // Part 1
     val part1 = part1(input)
     println(part1)
-    check(part1 == 1)
+    check(part1 == 296)
 
     // Part 2
+    // 617532994 no
     val part2 = part2(input)
     println(part2)
-    check(part2 == 1)
+    check(part2 == 619970556776002L)
 }
